912caf46e6f2b33ba9c61f37fcfa336f7e924269
David R Ingram <13292249+davidringram@users.noreply.github.com>
2026-01-13 17:22:13 -0800

    added extraCs

---
+++ src/data/extracurriculars.ts
@@ -0,0 +1,68 @@
+
+export const extracurriculars = [
+
+  {
+    title: "Google Analytics Individual Qualification",
+    institution: "Google",
+    description:
+      "Mastery of Google Analytics, including advanced reporting, audience analysis, and conversion optimization.",
+    tags: [
+      "Google Analytics",
+    ],
+  },
+  {
+    title: "Google Search and Display Ads Certification",
+    institution: "Google",
+    description:
+      "Mastery of Google Ads, including advanced reporting, audience analysis, and conversion optimization.",
+    tags: [
+      "Google Ads",
+      "PPC",
+    ],
+  },
+  {
+    title: "Google Tag Manager Certification",
+    institution: "Google",
+    description:
+      "Mastery of Google Tag Manager, including advanced reporting, audience analysis, and conversion optimization.",
+    tags: [
+      "Google Tag Manager",
+      "Tag Management",
+    ],
+  },
+  {
+    title: "Data Camp Python Certification",
+    institution: "Data Camp",
+    description:
+      "Mastery of Python, including advanced reporting, audience analysis, and conversion optimization.",
+    tags: [
+      "Python",
+      "Data Science",
+    ],
+  },
+  {
+    title: "HubSpot Inbound Marketing Certification",
+    institution: "HubSpot",
+    description:
+      "Mastery of HubSpot Inbound Marketing, including brand strategy, digital marketing, automation, analytics, attribution, and content marketing.",
+    tags: [
+      "HubSpot CRM",
+      "Inbound Marketing",
+    ],
+  },
+  {
+    title: "Desginated Marksman Course",
+    institution: "US Army. - 4ID Training",
+    description:
+      "Mastery of M14 Rifle, including qualification and maintenance.",
+    tags: [
+      "M14 Qualification",
+      "Stealth Operations",
+    ],
+  },
+
+
+];
+
+export type StudyItem = (typeof extracurriculars)[number];
+
